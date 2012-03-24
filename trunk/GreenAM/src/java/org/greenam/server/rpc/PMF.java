dc8776072bb7ae04106fddb6b7d9ca7d05b6eefc
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-03-24 22:45:38 +0000

    Added a Singelton helperclass for the PersistenceManger

---
+++ trunk/GreenAM/src/java/org/greenam/server/rpc/PMF.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenam.server.rpc;
+
+/**
+ *
+ * @author Emil
+ */
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+public final class PMF {
+    private static final PersistenceManagerFactory pmfInstance =
+        JDOHelper.getPersistenceManagerFactory("transactions-optional");
+
+    private PMF() {}
+
+    public static PersistenceManagerFactory get() {
+        return pmfInstance;
+    }
+}
