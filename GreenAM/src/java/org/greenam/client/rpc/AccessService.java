27cf49207fb2ddd2b3a9c495405627fad2fb9ffc
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-03-25 10:29:43 +0000

    Added an AccessService.

---
+++ GreenAM/src/java/org/greenam/client/rpc/AccessService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenam.client.rpc;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import org.greenam.client.rpc.exception.AccessException;
+
+/**
+ *
+ * @author Emil
+ */
+@RemoteServiceRelativePath("rpc/accessservice")
+public interface AccessService extends RemoteService {
+
+    public boolean hasAccess();
+    public boolean hasAccess(Long userId);
+    
+    public void setArtistStatus(String user, boolean b) throws AccessException;
+    public void setModeratorStatus(String user, boolean b) throws AccessException;
+}
