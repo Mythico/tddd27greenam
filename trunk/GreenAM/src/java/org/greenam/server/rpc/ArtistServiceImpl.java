fb4a284d44b16d1901316d4d25f7b3996fa3876f
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-16 10:51:30 +0000

    Added backend support for CalendarWidget

--- trunk/GreenAM/src/java/org/greenam/server/rpc/ArtistServiceImpl.java
+++ trunk/GreenAM/src/java/org/greenam/server/rpc/ArtistServiceImpl.java
@@ -4,9 +4,10 @@
  */
 package org.greenam.server.rpc;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Query;
+import java.util.Date;
 import java.util.List;
 import org.greenam.client.domain.*;
 
@@ -16,13 +17,16 @@ import org.greenam.client.rpc.ArtistService;
  *
  * @author Emil
  */
-public class ArtistServiceImpl extends RemoteServiceServlet implements ArtistService {
-    
-    Objectify ofy = ObjectifyService.begin();
-    
+public class ArtistServiceImpl extends ServiceImpl implements ArtistService {
+
     @Override
-    public List<Event> getEvents(Artist artist, int month) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<Event> getEvents(Artist artist) {
+        Objectify ofy = ObjectifyService.begin();
+
+        Date today = new Date();
+
+        List<Event> list = ofy.query(Event.class).filter("artistId", artist.getId()).filter("date >=", today).order("date").list();
+        return list;
     }
 
     @Override
@@ -31,22 +35,33 @@ public class ArtistServiceImpl extends RemoteServiceServlet implements ArtistSer
     }
 
     @Override
-    public void postEvent(Artist artist, Event event) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void postEvent(Event event) {
+        if (!hasAccess(event.artistId)) {
+            return; //TODO: Throw exception?
+        }
+        System.out.println("Posting " + event);
+        Objectify ofy = ObjectifyService.begin();
+        ofy.put(event);
     }
 
     @Override
-    public void postBlog(Artist artist, Blog blog) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void postBlog(Blog blog) {
+        if (!hasAccess(blog.artistId)) {
+            return; //TODO: Throw exception?
+        }
+        Objectify ofy = ObjectifyService.begin();
+        ofy.put(blog);
     }
 
     @Override
     public void save(Artist artist) {
+        Objectify ofy = ObjectifyService.begin();
         ofy.put(artist);
     }
 
     @Override
     public Artist update(Artist artist) {
+        Objectify ofy = ObjectifyService.begin();
         return ofy.get(Artist.class, artist.getId());
     }
 }
