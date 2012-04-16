8347e070eac1699baa7da6bdb22a98d68d5e15bc
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-16 10:05:16 +0000

    Added an Event class and a Calendar Widget. Also added some css.

--- GreenAM/src/java/org/greenam/client/domain/Event.java
+++ GreenAM/src/java/org/greenam/client/domain/Event.java
@@ -6,6 +6,7 @@ package org.greenam.client.domain;
 
 import com.googlecode.objectify.annotation.Entity;
 import java.util.Date;
+import javax.validation.constraints.NotNull;
 
 /**
  * A simple wrapper class containing a date and a short string
@@ -14,7 +15,39 @@ import java.util.Date;
 @Entity
 public class Event extends DatastoreObject {
     
+    public Long artistId;
     public Date date;
-    public String info;
+    public String message;
+
+    public Event() {
+    }
+   
+    public Event(Long artistId, Date date, String message) {
+        this.artistId = artistId;
+        this.date = date;
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public Long getArtistId() {
+        return artistId;
+    }
+    
+    
     
 }
