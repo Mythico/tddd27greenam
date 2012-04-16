37de1b425c7946b1e7a286fddc72f1820d53cd85
morawiec.michael@gmail.com <morawiec.michael@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-16 14:44:08 +0000

    resolved confict

--- GreenAM/src/java/org/greenam/client/domain/Blog.java
+++ GreenAM/src/java/org/greenam/client/domain/Blog.java
@@ -5,6 +5,7 @@
 package org.greenam.client.domain;
 
 import com.googlecode.objectify.annotation.Entity;
+import java.util.Date;
 
 /**
  *
@@ -13,9 +14,25 @@ import com.googlecode.objectify.annotation.Entity;
 @Entity
 public class Blog extends DatastoreObject {
      
-    public String content;
+    public String entry;
     public Long artistId;
+    private Date date;
     
     public Blog() {
+        this.entry = "";
+        this.date = null;
+    }
+    
+    public String getEntry() {
+        return entry;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+    
+    public void addEntry(String entry, Date date) {
+        this.entry = entry;
+        this.date = date;
     }
 }
