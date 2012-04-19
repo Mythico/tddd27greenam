9aca6a196d6d215d81d26da55d9dc92723343f4c
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-19 06:54:33 +0000

    Added a "real" search function. You can search by record title by searching for something. You can search for artist name by prefixing it with artist: NAME You can also search for artist Id by prefixing it with artistId: ID

--- trunk/GreenAM/src/java/org/greenam/client/rpc/RecordService.java
+++ trunk/GreenAM/src/java/org/greenam/client/rpc/RecordService.java
@@ -6,7 +6,12 @@ package org.greenam.client.rpc;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import org.greenam.client.domain.Album;
+import org.greenam.client.domain.Artist;
+import org.greenam.client.domain.LinkObject;
 import org.greenam.client.domain.Record;
 
 /**
@@ -19,4 +24,7 @@ public interface RecordService extends RemoteService {
     public List<Record> search(String s);
 
     String getBlobStoreUploadUrl();
+    
+    List<Album> getAlbums(Artist artist);
+    List<LinkObject<String>> getAlbumNamesFromRecords(List<Long> recordIds);
 }
