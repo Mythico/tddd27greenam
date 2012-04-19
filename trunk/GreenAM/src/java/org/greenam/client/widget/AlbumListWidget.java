630452c90ca1606d571b36b1b20fa05129c4cebb
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-19 06:58:20 +0000

    And I forgot to add a file to the last commit.

--- trunk/GreenAM/src/java/org/greenam/client/widget/AlbumListWidget.java
+++ trunk/GreenAM/src/java/org/greenam/client/widget/AlbumListWidget.java
@@ -4,6 +4,7 @@
  */
 package org.greenam.client.widget;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import java.util.List;
 import org.greenam.client.domain.Album;
@@ -19,23 +20,41 @@ public class AlbumListWidget extends ListWidget<Album> {
         super(viewController);
 
 
-        grid.resize(1, 2);
-        grid.setText(0, 1, "Name");
+        grid.resize(1, 3);
+        grid.setText(0, 0, "Name");
+        grid.setText(0, 1, "Records");
+        grid.setText(0, 2, "Artists");
     }
 
     @Override
     protected void update(List<Album> list) {
         int row = list.size() + 1;
-        grid.resize(row, 6);
+        grid.resize(row, 3);
 
         int i = 1;
         for (final Album album : list) {
             Label name = new Label(album.getTitle());
-
             name.setStyleName("gam-RecordListWidgetLink");
+            
+            HorizontalPanel rhp = new HorizontalPanel();
+            for(Long rid :album.getRecordIds()){
+                Label ridLabel = new Label("" + rid);
+                ridLabel.setStyleName("gam-RecordListWidgetLink");
+                rhp.add(ridLabel);
+            }
+            HorizontalPanel ahp = new HorizontalPanel();
+            for(Long aid : album.getArtistIds()){
+                
+                Label ridLabel = new Label("" + aid);
+                ridLabel.setStyleName("gam-RecordListWidgetLink");
+                ahp.add(ridLabel);
+            }
+
 
 
             grid.setWidget(i, 0, name);
+            grid.setWidget(i, 1, rhp);
+            grid.setWidget(i, 2, ahp);
             i++;
         }
     }
