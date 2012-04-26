b72c9fc353709af745a6d421a2b32d99794d7cb6
morawiec.michael@gmail.com <morawiec.michael@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-26 14:42:19 +0000

    added so current money is shown in loginwidget and temp fix on how its updated

--- trunk/GreenAM/src/java/org/greenam/client/widget/LoginWidget.java
+++ trunk/GreenAM/src/java/org/greenam/client/widget/LoginWidget.java
@@ -12,6 +12,8 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import javax.swing.ImageIcon;
 import org.greenam.client.domain.User;
 import org.greenam.client.rpc.UserService;
 import org.greenam.client.rpc.UserServiceAsync;
@@ -21,12 +23,16 @@ import org.greenam.client.view.ViewController;
  *
  * @author Emil
  */
-public class LoginWidget extends HorizontalPanel {
+public class LoginWidget extends VerticalPanel {
 
     private final Button loginButton = new Button("Login");
+    private final Button updateButton = new Button("Update");
     private final UserServiceAsync async = GWT.create(UserService.class);
     private final Label loginLabel = new Label();
+    private final Label moneyLabel = new Label();
     private final ViewController viewController;
+    private final HorizontalPanel hp = new HorizontalPanel();
+    private final HorizontalPanel hp2 = new HorizontalPanel();
 
     public LoginWidget(final ViewController viewController) {
         this.viewController = viewController;
@@ -38,10 +44,21 @@ public class LoginWidget extends HorizontalPanel {
             }
         });
         loginLabel.addStyleName("gam-Link");
-        add(loginLabel);
-        add(loginButton);
+        hp.add(loginLabel);
+        hp.add(loginButton);
+        add(hp);
+        hp2.add(moneyLabel);
+        hp2.add(updateButton);
+        add(hp2);
 
+        updateButton.addClickHandler(new ClickHandler() {
 
+            @Override
+            public void onClick(ClickEvent event) {
+                moneyLabel.setText(" Money: " + viewController.getUser().getMoney());
+            }
+        });
+        
         async.getCurrentUser(new AsyncCallback<User>() {
 
             @Override
@@ -54,10 +71,12 @@ public class LoginWidget extends HorizontalPanel {
                 if (result == null) { //No user is loggin
                     loginButton.setText("Login");
                     loginLabel.setText("");
+                    moneyLabel.setText("");
                     viewController.logout();
                 } else {
                     loginButton.setText("Logout");
                     loginLabel.setText("Logged in as: " + result.getName());
+                    moneyLabel.setText(" Money: " + viewController.getUser().getMoney());
                     loginLabel.addClickHandler(new ClickHandler() {
 
                         @Override
@@ -68,7 +87,7 @@ public class LoginWidget extends HorizontalPanel {
                 }
             }
         });
-
+        
 
     }
 }
