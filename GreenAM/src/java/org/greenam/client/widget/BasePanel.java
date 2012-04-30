16c1f1ed4c8a6fd46af27067b0843e0247b5b6a9
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-30 07:50:29 +0000

    Added a base panel that will be used by the helper classes so we can minimize repeating  code. Also updated AlbumListWidget to actually show some albums.

---
+++ GreenAM/src/java/org/greenam/client/widget/BasePanel.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenam.client.widget;
+
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * The base panel is a simple panel used by many helper classes in this 
+ * application. It has the common functionality that most of them use.
+ * This includes using the same css-style and the ability to delete a panel and 
+ * its content.
+ * @author Emil
+ * @author Michael
+ */
+public abstract class BasePanel extends HorizontalPanel {
+
+
+    public BasePanel() {
+        setStyleName("gam-Box");
+    }
+
+
+    /**
+     * Adds a delete button and connects it to a click handler.
+     * @param handler 
+     */
+    protected void addDeleteHandler(ClickHandler handler) {
+        Image remove = new Image("img/cross_script.png");
+        remove.addClickHandler(handler);
+        super.add(remove);
+    }
+    /**
+     * A callback used by the click handler for removing this panel when an
+     * artist has been removed.
+     */
+    protected final AsyncCallback removeThis = new AsyncCallback() {
+
+        @Override
+        public void onFailure(Throwable caught) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void onSuccess(Object result) {
+            removeFromParent();
+        }
+    };
+}
