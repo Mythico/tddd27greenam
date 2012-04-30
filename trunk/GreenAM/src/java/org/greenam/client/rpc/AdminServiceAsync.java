c3f50a121bd24b4b4382d47a707b9316e53490b6
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-04-30 09:15:45 +0000

    Added the ability for admins to remove albums and records.

--- trunk/GreenAM/src/java/org/greenam/client/rpc/AdminServiceAsync.java
+++ trunk/GreenAM/src/java/org/greenam/client/rpc/AdminServiceAsync.java
@@ -7,7 +7,9 @@ package org.greenam.client.rpc;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import java.util.List;
 import org.greenam.client.domain.AdminRequest;
+import org.greenam.client.domain.Album;
 import org.greenam.client.domain.Artist;
+import org.greenam.client.domain.Record;
 
 /**
  *
@@ -18,6 +20,10 @@ public interface AdminServiceAsync {
     public void makeArtist(AdminRequest request, AsyncCallback callback);
 
     public void deleteArtist(Artist artist, AsyncCallback callback);
+
+    public void deleteAlbum(Album album, AsyncCallback callback);
+    
+    public void deleteRecord(Record record, AsyncCallback callback);
 
     public void deleteRequest(AdminRequest request, AsyncCallback callback);
 
