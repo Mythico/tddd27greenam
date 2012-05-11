f45ef7bb1e91308fa9c31c9dc3ac54729f885215
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-05-11 13:33:19 +0000

    Moved the code around a bit and commented the code in loginwidget.

--- GreenAM/src/java/org/greenam/client/widget/LoginWidget.java
+++ GreenAM/src/java/org/greenam/client/widget/LoginWidget.java
@@ -12,16 +12,18 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import javax.swing.ImageIcon;
 import org.greenam.client.domain.User;
 import org.greenam.client.rpc.UserService;
 import org.greenam.client.rpc.UserServiceAsync;
 import org.greenam.client.view.ViewController;
 
 /**
+ * Login Widget act as a login button if the user isn't login or as a logout
+ * button and user status if the us login. You can also click on the user name
+ * to go the user page.
  *
  * @author Emil
+ * @author Michael
  */
 public class LoginWidget extends BaseWidget {
 
@@ -37,47 +39,26 @@ public class LoginWidget extends BaseWidget {
 
             @Override
             public void onClick(ClickEvent event) {
-                Window.Location.replace("/_ah/login_required");
+                String query = Window.Location.getQueryString();
+                Window.Location.replace("/_ah/login_required" + query);
             }
         });
         loginLabel.addStyleName("gam-Link");
+        loginLabel.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                viewController.setUserView();
+            }
+        });
+
         hp.add(loginLabel);
         hp.add(loginButton);
         add(hp);
         add(moneyLabel);
-        
-        viewController.registerForUserUpdates(this);
-        
-        async.getCurrentUser(new AsyncCallback<User>() {
 
-            @Override
-            public void onFailure(Throwable caught) {
-                setError("LoginWidget RPC throwable", caught.getLocalizedMessage());
-            }
+        async.getCurrentUser(getUser);
 
-            @Override
-            public void onSuccess(final User result) {
-                clearStatus();
-                if (result == null) { //No user is loggin
-                    loginButton.setText("Login");
-                    loginLabel.setText("");
-                    moneyLabel.setText("");
-                    viewController.logout();
-                } else {
-                    loginButton.setText("Logout");
-                    loginLabel.setText("Logged in as: " + result.getName());
-                    moneyLabel.setText(" Money: " + viewController.getUser().getMoney());
-                    loginLabel.addClickHandler(new ClickHandler() {
-
-                        @Override
-                        public void onClick(ClickEvent event) {
-                            viewController.setUserView();
-                        }
-                    });
-                }
-            }
-        });
-        
 
     }
 
@@ -85,6 +66,59 @@ public class LoginWidget extends BaseWidget {
     public void update(User user) {
         moneyLabel.setText(" Money: " + user.getMoney());
     }
-    
-    
+    /**
+     * Get user is an asynchronous call for getting the current user and set the
+     * shape of the login widget depending on the result. If no user is login
+     * the result will be null.
+     */
+    private final AsyncCallback<User> getUser = new AsyncCallback<User>() {
+
+        @Override
+        public void onFailure(Throwable caught) {
+            setError("LoginWidget RPC throwable", caught.getLocalizedMessage());
+        }
+
+        @Override
+        public void onSuccess(final User result) {
+            clearStatus();
+            register();
+            if (result == null) { //No user is loggin
+                setLoginView();
+            } else {
+                setLogoutView(result.getName(), result.getMoney());
+            }
+        }
+    };
+
+    /**
+     * A helper function for getUser to be able to register this widget 
+     * for user updates.
+     */
+    private void register() {
+        viewController.registerForUserUpdates(this);
+    }
+
+    /**
+     * Set login view will show a button with the name login and will logout any
+     * local user that is login.
+     */
+    private void setLoginView() {
+        loginButton.setText("Login");
+        loginLabel.setText("");
+        moneyLabel.setText("");
+        viewController.logout();
+    }
+
+    /**
+     * Set logout view shows a logout button and the name and money of the
+     * current user.
+     *
+     * @param name Name of the current user.
+     * @param money Amount of money the current user has.
+     */
+    private void setLogoutView(String name, int money) {
+        loginButton.setText("Logout");
+        loginLabel.setText("Logged in as: " + name);
+        moneyLabel.setText(" Money: " + money);
+    }
 }
