2e7c660291dd342df0c211bd5422e8c81b4cd666
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-05-11 13:45:50 +0000

    Commented the Upload Widget

--- GreenAM/src/java/org/greenam/client/widget/UploadWidget.java
+++ GreenAM/src/java/org/greenam/client/widget/UploadWidget.java
@@ -7,23 +7,20 @@ package org.greenam.client.widget;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FileUpload;
-import com.google.gwt.user.client.ui.FormPanel;
-import com.google.gwt.user.client.ui.Grid;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.*;
 import org.greenam.client.rpc.RecordService;
 import org.greenam.client.rpc.RecordServiceAsync;
 import org.greenam.client.view.ViewController;
 
-
 /**
- *
+ * The Upload Widget is used for uploading music files to the blobstore.
+ * 
  * @author Emil
+ * @author Michael
  */
-public class UploadWidget extends FormPanel {
+public class UploadWidget extends BaseWidget {
+
     private final RecordServiceAsync recordInfo = GWT.create(RecordService.class);
     private FileUpload fileUpload = new FileUpload();
     private TextBox artistsBox = new TextBox();
@@ -32,14 +29,13 @@ public class UploadWidget extends FormPanel {
     private TextBox priceBox = new TextBox();
     private Button uploadButton = new Button("Upload");
     private Grid grid = new Grid(5, 2);
-    private final ViewController viewController;
+    private FormPanel formPanel = new FormPanel();
 
     public UploadWidget(final ViewController viewController) {
-        
-        this.viewController = viewController;
-        
-        setEncoding(FormPanel.ENCODING_MULTIPART);
-        setMethod(FormPanel.METHOD_POST);
+        super(viewController);
+
+        formPanel.setEncoding(FormPanel.ENCODING_MULTIPART);
+        formPanel.setMethod(FormPanel.METHOD_POST);
 
 
 
@@ -65,26 +61,44 @@ public class UploadWidget extends FormPanel {
 
             @Override
             public void onClick(ClickEvent event) {
-                recordInfo.getBlobStoreUploadUrl(new AsyncCallback<String>() {
-
-                    @Override
-                    public void onFailure(Throwable caught) {
-                        throw new UnsupportedOperationException("Not supported yet.");
-                    }
-
-                    @Override
-                    public void onSuccess(String result) {
-                        String name = viewController.getArtist().getName();
-                        String additionalNames = artistsBox.getText();
-                        artistsBox.setText(name + "," + additionalNames);
-                        setAction(result);
-                        submit();
-                        reset();
-                    }
-                });
+                recordInfo.getBlobStoreUploadUrl(blobUrl);
             }
         });
 
-        add(grid);
+        formPanel.add(grid);
+        add(formPanel);
+    }
+    /**
+     * An asynchronous call for fetching BlobStore URL and the submitting the
+     * uploaded file if it passes the check.
+     */
+    private AsyncCallback<String> blobUrl = new AsyncCallback<String>() {
+
+        @Override
+        public void onFailure(Throwable caught) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void onSuccess(String result) {
+            if (checkConstrains()) {
+                String name = viewController.getArtist().getName();
+                String additionalNames = artistsBox.getText();
+                artistsBox.setText(name + "," + additionalNames);
+                formPanel.setAction(result);
+                formPanel.submit();
+                formPanel.reset();
+            } else {
+                //TODO: 
+            }
+        }
+    };
+
+    /**
+     * Check if all boxes all filed correctly before uploading the file.
+     */
+    private boolean checkConstrains() {
+        //TODO: add checks.
+        return true;
     }
 }
