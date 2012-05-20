acf16c0b40fe2ac320c2843e08c83a784de8642a
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-05-20 07:31:25 +0000

    Commented the code.

--- GreenAM/src/java/org/greenam/client/domain/Blog.java
+++ GreenAM/src/java/org/greenam/client/domain/Blog.java
@@ -8,7 +8,7 @@ import com.googlecode.objectify.annotation.Entity;
 import java.util.Date;
 
 /**
- *
+ * Blog is a datastore object containg an entry, a id of an artist and a date
  * @author Emil
  * @author Michael
  */
@@ -23,20 +23,38 @@ public class Blog extends DatastoreObject {
         
     }
     
+    /**
+     * Creates a blog with an entry and an artist.
+     * The dates is created automatically.
+     * @param entry A blog entry
+     * @param artist An artist.
+     */
     public Blog(String entry, Artist artist) {
         this.entry = entry;
         this.date = new Date();
         artistId = artist.getId();
     }
     
+    /**
+     * Gets the entry.
+     * @return An entry.
+     */
     public String getEntry() {
         return entry;
     }
     
+    /**
+     * Gets the artist id.
+     * @return An artist id.
+     */
     public Long getArtistId() {
         return artistId;
     }
     
+    /**
+     * Gets the date this blog was posted.
+     * @return A date.
+     */
     public Date getDate() {
         return date;
     }    
--- GreenAM/src/java/org/greenam/client/domain/Comment.java
+++ GreenAM/src/java/org/greenam/client/domain/Comment.java
@@ -1,21 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.greenam.client.domain;
 
 import com.googlecode.objectify.annotation.Entity;
 import java.util.Date;
 
 /**
- *
+ * A Comment is a datastore object that contains a posting date, a name of the
+ * poster, a blog id for blog this Comment belongs to and an entry.
  * @author Emil
  * @author Michael
  */
 @Entity
 public class Comment extends DatastoreObject {
      
-    private String comment;
+    private String entry;
     private Long blogId;
     private Date date;
     private String name;
@@ -24,25 +21,48 @@ public class Comment extends DatastoreObject {
         
     }
     
-    public Comment(String comment, Blog blog, String name) {
-        this.comment = comment;
+    /**
+     * Creates a Comment with an entry, blog this comment was posted on and
+     * the name of the poster.
+     * @param entry An entry.
+     * @param blog A blog.
+     * @param name A name.
+     */
+    public Comment(String entry, Blog blog, String name) {
+        this.entry = entry;
         this.date = new Date();
         blogId = blog.getId();
         this.name = name;
     }
     
+    /**
+     * Gets the entry.
+     * @return An entry.
+     */
     public String getEntry() {
-        return comment;
+        return entry;
     }
     
+    /**
+     * Gets the name of the user that posted the comment.
+     * @return A name.
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Gets the blog id this comment was posted on.
+     * @return A blog id.
+     */
     public Long getBlogId() {
         return blogId;
     }
     
+    /**
+     * Gets the date this comment was posted on.
+     * @return A date.
+     */
     public Date getDate() {
         return date;
     }
