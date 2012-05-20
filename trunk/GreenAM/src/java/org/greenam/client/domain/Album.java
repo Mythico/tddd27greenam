20c9a43ebaa0745fa572cb70951c0672c866e70f
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-05-20 07:13:11 +0000

    Commentaded the code and removed unused functions.

--- trunk/GreenAM/src/java/org/greenam/client/domain/Album.java
+++ trunk/GreenAM/src/java/org/greenam/client/domain/Album.java
@@ -1,17 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.greenam.client.domain;
 
 import com.googlecode.objectify.annotation.Entity;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 /**
- *
+ * Album is a datastore object that has a name and a list of record ids.
+ * 
  * @author Emil
+ * @author Michael
  */
 @Entity
 public class Album extends DatastoreObject {
@@ -22,30 +19,37 @@ public class Album extends DatastoreObject {
     public Album() {
     }
       
-    
-    
+    /**
+     * Creates a new album with a title and a record.
+     * @param title A title.
+     * @param recordId A record id.
+     */    
     public Album(String title, Long recordId) {
         this.title = title;
         recordIds = new LinkedList<Long>();
         recordIds.add(recordId);
     }
 
+    /**
+     * Gets the title of the album.
+     * @return A title.
+     */
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
+    /**
+     * Gets a list of records ids.
+     * @return A list containing record ids.
+     */
     public List<Long> getRecordIds() {
         return recordIds;
     }
 
-    public void setRecordIds(List<Long> recordIds) {
-        this.recordIds = recordIds;
-    }
-
+    /**
+     * Adds a record to this album.
+     * @param id A record id.
+     */
     public void addRecord(Long id) {
         recordIds.add(id);
     }
