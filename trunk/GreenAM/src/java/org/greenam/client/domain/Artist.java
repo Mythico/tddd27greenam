0c72e4d880791fad97ba1cf7e02f246d9112f186
martensson.emil@gmail.com <martensson.emil@gmail.com@527f01bc-460f-1ee4-8a26-9efd75c6969f>
2012-05-20 07:31:25 +0000

    Commented the code.

--- trunk/GreenAM/src/java/org/greenam/client/domain/Artist.java
+++ trunk/GreenAM/src/java/org/greenam/client/domain/Artist.java
@@ -1,17 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.greenam.client.domain;
 
 import com.googlecode.objectify.annotation.Entity;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Set;
 
 /**
- *
+ * Artist is a datastore object that stores a user id, a name and a biography.
  * @author Emil
+ * @author Michael
  */
 @Entity
 public class Artist extends DatastoreObject{
@@ -23,30 +17,52 @@ public class Artist extends DatastoreObject{
     public Artist() {
     }
 
+    /**
+     * Creates an artist from a user.
+     * @param userId A user id.
+     */
     public Artist(Long userId) {
         this.userId = userId;
         this.name = "Artist [" + getId() + "]";
         this.biography = "";
-        
-        //this.events = events;
     }
 
+    /**
+     * Gets the user id of this artist.
+     * @return A user id.
+     */
     public Long getUserId() {
         return userId;
     }
     
+    /**
+     * Gets this artist biography.
+     * @return A biography.
+     */
     public String getBiography() {
         return biography;
     }
 
+    /**
+     * Sets this artist biography.
+     * @param biography A biography.
+     */
     public void setBiography(String biography) {
         this.biography = biography;
     }
 
+    /**
+     * Gets the artists name.
+     * @return A name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the artists name.
+     * @param name A name.
+     */
     public void setName(String name) {
         this.name = name;
     }
